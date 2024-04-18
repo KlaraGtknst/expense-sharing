2a17a840f2afec1905919a43dfa71b987d46747f
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2024-04-18 10:49:20 +0200

    style: Clean up code

--- UI/exp-shar/src/App.js
+++ UI/exp-shar/src/App.js
@@ -1,14 +1,11 @@
 import TextField from "./components/TextField.tsx";
-import DynamicList from "./components/DynamicList.tsx";
 import InputNewData from "./components/InputNewData.tsx";
-import NumberInputBasic from "./components/NumberInput.tsx";
 import Fetch from "./components/fetch.jsx";
-import Input from "./components/Input.tsx";
 import * as PropTypes from "prop-types";
 import '@vaadin/horizontal-layout';
 import {Button} from "@mui/material";
 import {styled} from "@mui/system";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import * as React from "react";
 
 
@@ -21,7 +18,6 @@ TextField.propTypes = {
 };
 
 function clickMe() {
-  // alert("You clicked me!");
     // Simple POST request with a JSON body using fetch
     const requestOptions = {
         method: 'POST',
@@ -42,22 +38,21 @@ const ButtonToggle = styled(Button)`
   `}
 `;
 export default function App() {
+    const [newPeople, setPeople] = useState([]);
+
+    const addPeople = useCallback(() => {
+    setPeople((t) => [...t, "New Todo"]);
+  }, [newPeople]);
+
   return (
       <div className="App">
-          <h1>Hello CodeSandbox.</h1>
-          <h2>Start editing to see some magic happen!</h2>
-          <div><Fetch/></div>
+          <h1>Expense Sharing</h1>
+          <h2>WIP</h2>
+          <div><Fetch  people={newPeople} addPeople={addPeople} /></div>
           <vaadin-horizontal-layout theme="spacing padding">
-              {/*<div><TextField/></div>*/}
-              {/*<div><Input/></div>*/}
-              {/*<div><NumberInputBasic/></div>*/}
-              {/*<div><DynamicList/></div>*/}
-                <div><InputNewData/></div>  # TODO: callback an class und dann zu fetch
-              {/*<div>*/}
-              {/*    <Button onClick={clickMe}>*/}
-              {/*        Pink theme*/}
-              {/*    </Button>*/}
-              {/*</div>*/}
+
+                <div><InputNewData/></div>  # TODO: callback an class und dann zu fetch: https://react.dev/learn/passing-props-to-a-component
+
           </vaadin-horizontal-layout>
 
       </div>
--- UI/exp-shar/src/components/fetch.jsx
+++ UI/exp-shar/src/components/fetch.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
+import InputNewData from "./InputNewData.tsx";
+import * as React from "react";
 
-const Fetch = () => {
+const Fetch = ({newPeople, addPeople}) => {
   const [people, setPeople] = useState([]);
   useEffect(() => {
     fetch('http://127.0.0.1:5000/settings')
@@ -11,14 +13,22 @@ const Fetch = () => {
         setPeople(data)
       });
   }, []);
+
+  const addToList = (newEntry) => {
+    // Add new entry to the list
+    setPeople([...people, newEntry]);
+  };
+
   return (
       <div>
-        <h1>People</h1>
-        <ul>
-          {people.map((person, index) => (
-              <li key={person._id}>{person.name}</li>
-          ))}
-        </ul>
+          <h1>People</h1>
+          <ul>
+              {people.map((person, index) => (
+                  <li key={person._id}>{person.name} paid {person.expense}</li>
+              ))}
+          </ul>
+          {/*<InputNewData addToList={addToList()}/>*/}
+
       </div>
   );
 };
